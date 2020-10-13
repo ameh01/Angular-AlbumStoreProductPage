16bda861e41222a71e96eeef03f2972f27dd75a2
Friday Ameh Matthew <48910588+ameh01@users.noreply.github.com>
2020-10-13 08:38:47 +0100

    Service for Album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private _http: Http) {}
+}
+
+class _albumUrl {
+  constructor(parameters) {
+
+  }
+}
